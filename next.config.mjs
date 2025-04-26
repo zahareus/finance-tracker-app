2edb1168f46ad63e5e03e4b0d479c1f8d5e53886
zahareus <zahareus@gmail.com>
2025-04-26 16:39:24 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true, // Можна увімкнути при потребі
+  // reactStrictMode: true,
+  // Додаємо просто коментар, щоб змінити файл
 };
 
 export default nextConfig;
